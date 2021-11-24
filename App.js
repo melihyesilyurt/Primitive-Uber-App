225f5251050ea45125ae5b00e094149d1686c065
MelihYesilyurt <180202060@kocaeli.edu.tr>
2021-11-24 20:08:34 +0300

    Implementing React Native Navigation is over but it doesn't work

--- App.js
+++ App.js
@@ -1,13 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Provider } from 'react-redux'
+import HomeScreen from './screens/HomeScreen';
+//import { Provider } from 'react';
+import { store } from './store';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      <NavigationContainer>
+      <SafeAreaProvider>
+        <HomeScreen />
+      </SafeAreaProvider>
+      </NavigationContainer>
+    </Provider>
   );
 }
 
