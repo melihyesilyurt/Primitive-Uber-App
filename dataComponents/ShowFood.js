5aba9ed7f9bdfa8bce14cf0edc01b665e93eca0f
MelihYesilyurt <180202060@kocaeli.edu.tr>
2021-12-07 16:39:11 +0300

    Asyncsotrage is finished

--- dataComponents/ShowFood.js
+++ dataComponents/ShowFood.js
@@ -1,11 +1,75 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React, {useEffect} from 'react'
+import { StyleSheet, Text, View, Image,FlatList,TouchableOpacity } from 'react-native'
 import tw from "tailwind-react-native-classnames";
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import useState from 'react-hook-use-state';
+
+const data= [
+    {
+        id: "789",
+    },
+ ];
 
 const ShowFood = () => {
+    const [destination, setdestination] = useState("");
+    const [travelTime, settravelTime] = useState(0);
+    const [price, setprice] = useState(0);
+    const [title, settitle] = useState("");
+    var prices= 0;
+    var travelTimes= 0;
+    var destinations= "";
+    var titles = "";
+
+    const clickhandler = () => {
+        settitle(titles);
+        setprice(prices);
+        setdestination(destinations);
+        settravelTime(travelTimes);
+      }
+
+    const load = async () => {
+        try {
+            let jsonValue = await AsyncStorage.getItem("Foodinfo")
+            if(jsonValue != null)
+            {
+                titles=JSON.parse(jsonValue).title;
+                prices=JSON.parse(jsonValue).price;
+                travelTimes=JSON.parse(jsonValue).travelTime;
+            }
+        } catch (error) {
+          // Error retrieving data
+        }
+        try {
+          let jsonValues = await AsyncStorage.getItem("FoodRoadinfo")
+          if(jsonValues != null)
+          {
+            destinations=JSON.parse(jsonValues).origin;
+          }
+      } catch (error) {
+        // Error retrieving data
+      }
+      {clickhandler()};
+      };
+
+    useEffect(function() {
+        {load()}
+    });
     return (
-        <View style = {tw`bg-yellow-200 flex-1`}>
+        <View style = {tw`bg-white flex-1`}>
             <Text style={tw`text-center py-5 text-xl`}> Your Previous Food Order </Text>
+            <FlatList data={data} 
+            renderItem={() => (
+            <TouchableOpacity style={tw`flex-row items-center p-5`} >
+                    <View style ={tw`-ml-6`}>
+                        <Text style={tw `text-xl font-semibold`}>  {title}</Text>
+                        <Text style = {tw`text-xl font-semibold`}>   {price} TL </Text>
+                    </View>
+                    <View>
+                        <Text style = {tw`text-xl font-semibold`}>  Travel Time: {travelTime}</Text>      
+                        <Text style = {tw`font-semibold`}>  Destination: {destination} </Text>
+                    </View>
+            </TouchableOpacity>
+    )} />       
         </View>
     )
 }
--- dataComponents/ShowRide.js
+++ dataComponents/ShowRide.js
@@ -2,56 +2,40 @@ import React, {useEffect} from 'react'
 import { StyleSheet, Text, View, Image,FlatList,TouchableOpacity } from 'react-native'
 import tw from "tailwind-react-native-classnames";
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import { Icon } from "react-native-elements";
 import useState from 'react-hook-use-state';
 const data= [
    {
        id: "789",
-       icon: "location",
-       location: "Your Location",
-       destination: "Your current location",
    },
 ];
 const ShowRide = () => {
-    //var titles= "hi";
     const [travelTime, settravelTime] = useState(0);
     const [price, setprice] = useState(0);
-    const [title, settitle] = useState("ali");
+    const [title, settitle] = useState("");
     const [start, setstart] = useState("");
     const [finish, setfinish] = useState("");
-    const [car, setcar] = useState("https://links.papareact.com/7pf");
-    const prices= 0;
-    const traveltimes= 0;
-    const starts= "";
-    const finishs= "";
-    const cars ="https://links.papareact.com/7pf";
-    const titles = "";
+    var prices= 0;
+    var traveltimes= 0;
+    var starts= "";
+    var finishs= "";
+    var titles = "";
 
     const clickhandler = () => {
-      //settitle(titles);
+      settitle(titles);
       setprice(prices);
       setstart(starts);
       setfinish(finishs);
       settravelTime(traveltimes);
-      setcar(cars);
     }
     const load = async () => {
       try {
           let jsonValue = await AsyncStorage.getItem("Carinfo")
           if(jsonValue != null)
           {
-            console.log("çalışıyor")
-              console.log(JSON.parse(jsonValue).title)
-              //console.log(JSON.parse(jsonValue).title)
-              //titles=JSON.parse(jsonValue).title;
-              //prices=JSON.parse(jsonValue).price;
-              //settitle(JSON.parse(jsonValue).title);
-              //console.log(title);
-              //console.log(prices)
-              //traveltimes=JSON.parse(jsonValue).traveltime;
-              //cars=JSON.parse(jsonValue).Image;
+              titles=JSON.parse(jsonValue).title;
+              prices=JSON.parse(jsonValue).price;
+              traveltimes=JSON.parse(jsonValue).travelTime;
           }
-      
       } catch (error) {
         // Error retrieving data
       }
@@ -59,42 +43,33 @@ const ShowRide = () => {
         let jsonValues = await AsyncStorage.getItem("Roadinfo")
         if(jsonValues != null)
         {
-          //console.log(JSON.parse(jsonValues).destination)
+          starts=JSON.parse(jsonValues).origin;
+          finishs=JSON.parse(jsonValues).destination;
         }
     } catch (error) {
       // Error retrieving data
     }
+    {clickhandler()};
     };
     
     useEffect(function() {
       {load()}
-      {clickhandler()}
     });
     
     return (
-        <View style = {tw`bg-green-300 flex-1`}>
+        <View style = {tw`bg-white flex-1`}>
             <Text style={tw`text-center py-5 text-xl`}> Your Previous Journey </Text>
             <FlatList data={data} 
             renderItem={() => (
             <TouchableOpacity style={tw`flex-row items-center p-5`} >
-              
-                <Image
-                        style={{
-                            width: 100,
-                            height: 100,
-                            resizeMode: "contain",
-                        }}
-                        source={{ uri: car }}
-                        />
                     <View style ={tw`-ml-6`}>
-                        <Text style={tw `text-xl font-semibold`}>  {title} title</Text>
+                        <Text style={tw `text-xl font-semibold`}>  {title}</Text>
                         <Text style = {tw`text-xl font-semibold`}>   {price} TL </Text>
                     </View>
                     <View>
-                        <Text style = {tw`text-xl font-semibold`}>  Travel Time: {travelTime}</Text>
-                        
-                        <Text style = {tw`text-xl font-semibold`}>  Start: {start} </Text>
-                        <Text style = {tw`text-xl font-semibold`}>  Finish: {finish} </Text>
+                        <Text style = {tw`text-xl font-semibold`}>  Travel Time: {travelTime}</Text>      
+                        <Text style = {tw`font-semibold`}>  Start: {start} </Text>
+                        <Text style = {tw`font-semibold`}>  Finish: {finish} </Text>
                     </View>
             </TouchableOpacity>
     )} />       
@@ -103,5 +78,4 @@ const ShowRide = () => {
 }
 
 export default ShowRide
-
 const styles = StyleSheet.create({})
